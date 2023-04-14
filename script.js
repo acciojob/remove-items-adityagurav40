8ca2ec972613a93749cde0906dad1efd265768f2
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-04-14 12:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeColor() {
+  // Get dropdown list element
+  var dropdown = document.getElementById("colorSelect");
+  
+  // Get selected option index
+  var selectedOptionIndex = dropdown.selectedIndex;
+  
+  // Remove selected option from dropdown list
+  dropdown.remove(selectedOptionIndex);
+}
